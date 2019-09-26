9d7b58be0324e04429893e22cc67aef391e75182
Vaidios <25388414+Vaidios@users.noreply.github.com>
2019-09-26 10:13:49 +0200

    BETTER AND IMPROVED INPUT FORMAT

--- DAT159CryptoLab/src/no/hvl/dat159/client/TCPClient.java
+++ DAT159CryptoLab/src/no/hvl/dat159/client/TCPClient.java
@@ -29,43 +29,73 @@ public class TCPClient {
 		Vignere cypher = new Vignere("BITTERLEMON");
 		try {
 			Socket csocket = new Socket(server, port);
-			
-			PrintWriter outmsg = new PrintWriter(csocket.getOutputStream(), true);
 			BufferedReader inmsg = new BufferedReader(new InputStreamReader(csocket.getInputStream()));
 			System.out.println("______________________________________");
 			System.out.println("Message to TCPServer: "+ msg);
 			System.out.println("Message to TCPServer: "+ cypher.encrypt(msg));
 			System.out.println("______________________________________");
-			outmsg.println(cypher.encrypt(msg));
-			StringBuffer sb = new StringBuffer();
-			while((outtxt = inmsg.readLine()) != null) {
-				sb.append(outtxt+"\n");
+			this.sendMessege(csocket, msg);
+			if(csocket.isClosed() == false) {
+				this.getResponse(csocket);
 			}
-			System.out.println("______________________________________");
-			System.out.println("[Response from TCPServer]: "+sb);
-			System.out.println("Decrypted: "+ cypher.decrypt(sb.toString()));
-			System.out.println("______________________________________");
-			outmsg.close();
-			inmsg.close();
+//			StringBuffer sb = new StringBuffer();
+//			while((outtxt = inmsg.readLine()) != null) {
+//				sb.append(outtxt+"\n");
+//			}
+//			System.out.println("______________________________________");
+//			System.out.println("[Response from TCPServer]: "+sb);
+//			System.out.println("Decrypted: "+ cypher.decrypt(sb.toString()));
+//			System.out.println("______________________________________");
+//			inmsg.close();
 			csocket.close();
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+
 	}
 
+	public void sendMessege(Socket sSocket, String messege) {
+		try {
+			PrintWriter outmsg = new PrintWriter(sSocket.getOutputStream(), true);
+			outmsg.println(messege);
+			//outmsg.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	public void getResponse(Socket serverSocket) {
+		try {
+			BufferedReader inmsg = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+			String inputMsg;
+			while((inputMsg = inmsg.readLine()) != null) {
+				int lastIndexOfHeader = inputMsg.lastIndexOf("n") + 1;
+				String httpHeader = inputMsg.substring(0, lastIndexOfHeader);
+				String messege = inputMsg.substring(lastIndexOfHeader, inputMsg.length());
+				System.out.println(httpHeader);
+				System.out.println(messege);
+			}
+			inmsg.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
 	
 	public static void main(String[] args) {
-		Timer timer = new Timer();
-		
-		timer.scheduleAtFixedRate(new TimerTask() {
-			@Override
-			public void run() {
-				TCPClient c = new TCPClient(ServerConfig.SERVER, ServerConfig.PORT);
-				c.clientProcess("CLIENTXXXEncryptedXXXMessege");
-			}
-		}, 0, 20 * 1000);
+//		Timer timer = new Timer();
+		TCPClient c = new TCPClient(ServerConfig.SERVER, ServerConfig.PORT);
+		c.clientProcess("CLIENTXXXEncryptedXXXMessege");
+//		timer.scheduleAtFixedRate(new TimerTask() {
+//			@Override
+//			public void run() {
+//				
+//			}
+//		}, 0, 20 * 1000);
 	}
 
 }
